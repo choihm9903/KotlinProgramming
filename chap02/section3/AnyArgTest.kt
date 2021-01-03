f77df362127ce5bb67cd1ec5495620936b495edd
hyunmin <choihm9903@naver.com>
2021-01-03 21:37:04 +0900

    chap02

---
+++ chap02/section3/AnyArgTest.kt
@@ -0,0 +1,15 @@
+package chap02.section3
+
+fun main() {
+    checkArg("Hello")
+    checkArg(5)
+}
+
+fun checkArg(x: Any) {
+    if (x is String)
+        println("x is String: $x")
+    else if (x is Int)
+        println("x is Int: $x")
+    else
+        println("x is not String and Int")
+}
