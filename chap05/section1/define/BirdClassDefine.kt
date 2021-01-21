b979a65fe45cb085cc0efd6426ca65ba8efca016
unknown <choihm9903@naver.com>
2021-01-22 00:16:11 +0900

    README

--- chap05/section1/define/BirdClassDefine.kt
+++ chap05/section1/define/BirdClassDefine.kt
@@ -1,2 +1,22 @@
 package chap05.section1.define
 
+class Bird {    // 클래스의 정의
+    // 프로퍼티(속성)
+    var name: String ="mybird"
+    var wing: Int = 2
+    var beak: String = "short"
+    var color: String = "blue"
+
+    // 메소드(함수)
+    fun fly() = println("Fly wing: $wing")
+    fun sing(vol: Int) = println("Sing vol: $vol")
+}
+
+fun main() {
+    val coco = Bird()
+    coco.color = "blue"
+
+    println("coco.color: ${coco.color}")
+    coco.fly()
+    coco.sing(3)
+}
--- chap05/section2/init/BirdPrimaryInit.kt
+++ chap05/section2/init/BirdPrimaryInit.kt
@@ -1,2 +1,23 @@
 package chap05.section2.init
 
+class Bird (var name: String, var wing: Int, var beak: String, var color: String) {
+    // 초기화 블록
+    init {
+        println("--------초기화 블록 시작--------")
+        println("이름은 $name, 부리는 $beak")
+        this.sing(3)
+        println("-------- 초기화 블록 끝 --------")
+    }
+
+    // 메소드(함수)
+    fun fly() = println("Fly wing: $wing")
+    fun sing(vol: Int) = println("Sing vol: $vol")
+}
+
+fun main() {
+    val coco = Bird("mybird", 2, "short", "blue")
+    coco.color = "yellow"
+
+    println("coco.color: ${coco.color}")
+    coco.fly()
+}
