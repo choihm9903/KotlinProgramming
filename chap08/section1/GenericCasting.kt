e23a950b811a0371aa5fac4d7727fd013af0e7fc
Hyunmin <choihm9903@naver.com>
2021-03-06 14:27:13 +0900

    chap08-1/GenericCasting

---
+++ chap08/section1/GenericCasting.kt
@@ -0,0 +1,18 @@
+package chap08.section1
+
+open class Parent
+
+class Child: Parent()
+
+class Cup<T>
+
+fun main() {
+    val obj1: Parent = Child()
+    // val obj2: Child = Parent() 자료형 불일치 오류
+
+    // val obj3: Cup<Parent> = Cup<Child>() 자료형 불일치 오류
+    // val obj4: Cup<Child> = Cup<Parent>() 자료형 불일치 오류
+
+    val obj5 = Cup<Child>()
+    val obj6: Cup<Child> = obj5
+}
